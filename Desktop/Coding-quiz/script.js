28d03e06ac3f9285630a3e5e4cd9c84ac17d7bd2
Coyotefrank <franklopez35789@gmail.com>
2023-06-30 14:59:17 -0700

    added starter code for js and html

---
+++ Desktop/Coding-quiz/script.js
@@ -0,0 +1,7 @@
+//global variables that 
+var score = 0;
+var timeleft = 200;
+var questions = [];
+var answerChoices = [["...", "...", "... ", "...",]]; //made an array of strings, need to fill with dofferent choices for the quiz
+var correctAnswer = []; //made of strins
+
